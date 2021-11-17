cc2f68d08890703c490f7c7903d08e3d2b4cc879
jtardioli <joshua.tardioli@gmail.com>
2021-11-17 16:50:34 -0500

    Add createTweetElement and renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,5 +3,95 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 
+$(document).ready(function() {
+  const createTweetElement = function(tweetObj) {
+    const created = tweetObj.created_at;
+    const $tweet = $(`
+
+    <article class="tweet">
+    <header>
+      <div >
+        <img src="${tweetObj.user.avatars}" alt="">
+        <p>${tweetObj.user.name}</p>
+      </div>
+      <p>${tweetObj.user.handle}</p>
+      </header>
+      <div class="tweet-content">
+        <p>${tweetObj.content.text}</p>
+      </div>
+     
+      <footer>
+        <p>${timeago.format(created)}</p>
+        <div>
+          <i class="far fa-flag"></i> 
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+
+    `);
+
+    return $tweet;
+  };
+
+  const renderTweets = function(tweetsArr) {
+    for (let tweetData of tweetsArr) {
+      const $tweet = createTweetElement(tweetData);
+      $('main').append($tweet);
+    }
+  };
+  
+  
+  renderTweets(data);
+  
+  
+});
+
+// <article class="tweet">
+// <header>
+//   <div >
+//     <img src="https://i.imgur.com/73hZDYK.png" alt="">
+//     <p>Newton</p>
+//   </div>
+//   <p>@SirIsaac</p>
+//   </header>
+//   <div class="tweet-content">
+//     <p>If I have seen further it is by standing on the shoulders of giants</p>
+//   </div>
+ 
+//   <footer>
+//     <p>10 days ago</p>
+//     <div>
+//       <i class="far fa-flag"></i>
+//       <i class="fas fa-retweet"></i>
+//       <i class="far fa-heart"></i>
+//     </div>
+//   </footer>
+// </article>
