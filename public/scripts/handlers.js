da07d5f798ede0928d0d7392e812d38ae6394b30
jtardioli <joshua.tardioli@gmail.com>
2021-11-17 22:48:50 -0500

    add animation for write tweet button

--- public/scripts/handlers.js
+++ public/scripts/handlers.js
@@ -6,6 +6,16 @@ $(() => {
 
   // Write tweet button
   $('.fa-angle-double-down').click(function() {
+    console.log('hi');
+    if ($('.fa-angle-double-down').hasClass('rotate')) {
+      $('.fa-angle-double-down').removeClass('rotate');
+      $('.new-tweet').slideUp();
+    } else {
+      $('.fa-angle-double-down').addClass('rotate');
+      $('.new-tweet').slideDown('slow');
+    }
+    
+    
     
   });
 
